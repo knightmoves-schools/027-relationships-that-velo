d0385b140cd79ce63508e600af3195738db78077
that.slow.veloster <jadirks@anamosa.k12.ia.us>
2026-05-08 16:18:00 +0000

    lool

--- exercise.sql
+++ exercise.sql
@@ -1,1 +1,6 @@
 -- Add your SQL here
+SELECT Employee.ID, Employee.NAME, Contact_Info.EMAIL, Contact_Info.PHONE_NUMBER, Stores.LOCATION, Stores.REVENUE
+FROM Employee
+INNER JOIN Contact_Info ON Employee.ID = Contact_Info.ID
+INNER JOIN Stores ON Employee.LOCATION = Stores.LOCATION
+WHERE Stores.LOCATION IN ('Portland', 'Seattle', 'Los Angeles', 'San Francisco');
