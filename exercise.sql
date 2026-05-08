525d9a8f224531344ec94656112a21353c320b57
that.slow.veloster <jadirks@anamosa.k12.ia.us>
2026-05-08 16:26:11 +0000

    lllllllllllllllllllllllll

--- exercise.sql
+++ exercise.sql
@@ -3,4 +3,5 @@ SELECT Employee.ID, Employee.NAME, Contact_Info.EMAIL, Contact_Info.PHONE_NUMBER
 FROM Employee
 INNER JOIN Contact_Info ON Employee.ID = Contact_Info.ID
 INNER JOIN Stores ON Employee.LOCATION = Stores.LOCATION
-WHERE Stores.LOCATION IN ('Portland', 'Seattle', 'Los Angeles', 'San Francisco');
+WHERE Stores.LOCATION IN ('Portland', 'Seattle', 'Los Angeles', 'San Francisco')
+ORDER BY Employee.ID;
